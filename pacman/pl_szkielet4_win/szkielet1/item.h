aff3396687ac8c649c00740fdf272b8a27f57392
implikacja <agnieszka.l1995@gmail.com>
2016-05-31 14:37:46 +0200

    + obiekty w klasach
    jak wyżej

--- pacman/pl_szkielet4_win/szkielet1/item.h
+++ pacman/pl_szkielet4_win/szkielet1/item.h
@@ -1,13 +1,20 @@
 #pragma once
 
 #include "includes.h"
+#include "point.h"
 using namespace glm;
 class item
 {
 public:
-	int x;
+	point pos;
+	mat4 P;
+	mat4 V;
+	mat4 M;
+
 	item();
 	~item();
 	void drawObject(GLuint vao, ShaderProgram *shaderProgram, mat4 mP, mat4 mV, mat4 mM);
+	virtual void nextFrame();
+	virtual void logicTic();
 };
 
