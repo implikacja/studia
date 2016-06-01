d2c8f67c7013264183ec2e331853f95fafdd2a7e
Amarylian <lucja.m.lucy@gmail.com>
2016-06-01 14:54:43 +0200

    Wczytywanie pliku .obj
    + funkcja loadObj(...) w config

--- pacman/pl_szkielet4_win/szkielet1/config.cpp
+++ pacman/pl_szkielet4_win/szkielet1/config.cpp
@@ -75,7 +75,22 @@ config::config()
 
 						  //******Koniec przygotowania obiektu************
 
+	std::vector< glm::vec3 > vertices;
+	std::vector< glm::vec3 > normals; // Won't be used at the moment.
+	bool res = loadObj("cube.obj", vertices, normals);
 
+	if (res)
+	{
+		for (int i = 0; i < vertices.size(); i++)
+		{
+			printf("Wierzcholek %d: %f\t%f\t%f\n", i, vertices[i].x, vertices[i].y, vertices[i].z);
+		}
+
+		for (int i = 0; i < vertices.size(); i++)
+		{
+			printf("Wierzcholek normalny %d: %f\t%f\t%f\n", i, normals[i].x, normals[i].y, normals[i].z);
+		}
+	}
 
 }
 
@@ -152,4 +167,79 @@ void config::mainloop(world w)
 
 		glfwPollEvents(); //Wykonaj procedury callback w zaleznoœci od zdarzeñ jakie zasz³y.
 	}
+}
+
+bool config::loadObj(const char * path, std::vector < glm::vec3 > & out_vertices, std::vector < glm::vec3 > & out_normals)
+{
+	std::vector<unsigned int> vertexIndices, normalIndices;
+	std::vector<glm::vec3> temp_vertices;
+	std::vector<glm::vec3> temp_normals;
+
+	FILE * file = fopen(path, "r");
+	if (file == NULL) {
+		printf("Nie po¿na otworzyæ pliku %s!\n", path);
+		return false;
+	}
+
+	while (1)
+	{
+
+		char lineHeader[128];
+		int res = fscanf(file, "%s", lineHeader);
+		if (res == EOF) break;
+
+		if (strcmp(lineHeader, "v") == 0)
+		{
+			glm::vec3 vertex;
+			fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+			temp_vertices.push_back(vertex);
+		}
+		else if (strcmp(lineHeader, "vn") == 0)
+		{
+			glm::vec3 normal;
+			fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+			temp_normals.push_back(normal);
+		}
+		else if (strcmp(lineHeader, "f") == 0)
+		{
+			std::string vertex1, vertex2, vertex3;
+			unsigned int vertexIndex[3], normalIndex[3];
+			int matches = fscanf(file, "%d//%d %d//%d %d//%d\n", &vertexIndex[0], &normalIndex[0], &vertexIndex[1], &normalIndex[1], &vertexIndex[2], &normalIndex[2]);
+			if (matches != 6)
+			{
+				printf("%s: niew³aœciwy format face w pliku. Sprobuj u¿yæ innej metody loadObj\n", path);
+				return false;
+			}
+			vertexIndices.push_back(vertexIndex[0]);
+			vertexIndices.push_back(vertexIndex[1]);
+			vertexIndices.push_back(vertexIndex[2]);
+			normalIndices.push_back(normalIndex[0]);
+			normalIndices.push_back(normalIndex[1]);
+			normalIndices.push_back(normalIndex[2]);
+		}
+		else
+		{
+			printf("%s: niew³aœciwy format w pliku. Znaleziono symbol inny ni¿ v, vn, f\n", path);
+			return false;
+		}
+
+	}
+
+	for (unsigned int i = 0; i < vertexIndices.size(); i++)
+	{
+		unsigned int vertexIndex = vertexIndices[i];
+		glm::vec3 vertex = temp_vertices[vertexIndex - 1]; //-1,  bo w .obj wierzcho³ki s¹ indeksowane od 1
+		out_vertices.push_back(vertex);
+	}
+
+
+	for (unsigned int i = 0; i < normalIndices.size(); i++)
+	{
+		unsigned int normalIndex = normalIndices[i];
+		glm::vec3 normal = temp_normals[normalIndex - 1]; //-1,  bo w .obj wierzcho³ki s¹ indeksowane od 1
+		out_normals.push_back(normal);
+	}
+
+	return true;
+
 }
--- pacman/pl_szkielet4_win/szkielet1/config.h
+++ pacman/pl_szkielet4_win/szkielet1/config.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "includes.h"
+#include <vector>
 
 class world;
 class config
@@ -25,5 +26,7 @@ public:
 	static void error_callback(int error, const char* description);
 	static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 	void mainloop(world w);
+
+	bool loadObj(const char * path, std::vector < glm::vec3 > & out_vertices, std::vector < glm::vec3 > & out_normals);
 };
 
